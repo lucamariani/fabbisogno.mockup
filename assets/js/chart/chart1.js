2be65cd2b339f498c8ce67d7609cb5db55998d72
Luca Mariani <luca.marianj@gmail.com>
2021-12-20 23:48:42 +0100

    WIP: working plotly

--- assets/js/chart/chart1.js
+++ assets/js/chart/chart1.js
@@ -1,5 +1,61 @@
-console.log('csv',CSV_ENDPOINT)
+const fteAsIsID = 'fte_as_is_italy';
+const fteToBeID = 'fte_to_be_italy';
 
-d3.csv(CSV_ENDPOINT, function(err, rows) {
-    getTraces(rows)
+let regioni = []
+let fteAsIsItalyValue;
+let fteToBeItalyValue;
+
+Plotly.d3.csv(CSV_ENDPOINT, function(err, rows) {
+    regioni = createRegioniArray(rows)
+    fteAsIsItalyValue = aggregateValues(regioni, 'fteasis')
+    fteToBeItalyValue = aggregateValues(regioni, 'ftetobe')
+    // set ftes
+    setItalyFTE()
 })
+
+const makePlot = function() {
+    const data = [trace1, trace2];
+    const layout = {barmode: 'stack'};
+    Plotly.newPlot('myDiv', data, layout);
+}
+
+/**
+ * Parse rows and create array with regione objects.
+ * @param rows
+ * @returns {*|[]}
+ */
+const createRegioniArray = function (rows) {
+    const regioni = getRegioni(rows)
+    $.each(regioni, function (index, regione) {
+        console.debug(regione)
+        regione.fteasis = aggregate(rows, CODICE_REGIONE_KEY, regione.codice, FTE_AS_IS_KEY);
+        regione.ftetobe = aggregate(rows, CODICE_REGIONE_KEY, regione.codice, FTE_TO_BE_KEY);
+    })
+    return regioni;
+}
+
+const setItalyFTE = function () {
+    $fteAsIs = $('#' + fteAsIsID)
+    $fteAsIs.text(numberWithDots(fteAsIsItalyValue))
+
+    $fteToBe = $('#' + fteToBeID)
+    $fteToBe.text(numberWithDots(fteToBeItalyValue))
+}
+
+/**
+ * Sum all the aggregateField values for items with key aggregateOnKey equals to aggregateOnValue
+ * @param data_array
+ * @param aggregateOnKey
+ * @param aggregateOnValue
+ * @param aggregateField
+ */
+const aggregate = function (data_array, aggregateOnKey, aggregateOnValue, aggregateField) {
+    const arrayFilteredOnKeys = data_array.filter((value) =>  value[aggregateOnKey] === aggregateOnValue);
+    return aggregateValues(arrayFilteredOnKeys, aggregateField)
+}
+
+const aggregateValues = function (data_array, aggregateField) {
+    const values = unpack(data_array, aggregateField)
+    console.debug('values', values)
+    return values.reduce((acc,curr) => parseFloat(acc) + parseFloat(curr));
+}
--- assets/js/chart/common.js
+++ assets/js/chart/common.js
@@ -1,3 +1,66 @@
+const CODICE_REGIONE_KEY = 'Codice Regione';
+const REGIONE_KEY = 'Descrizione Regione';
+const FTE_AS_IS_KEY = 'FTE AS IS';
+const FTE_TO_BE_KEY = 'FTE TO BE';
+
+const numberWithCommas = function(x) {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
+const getUniqueValues = (rows) => rows.reduce( function(acc,curr) {
+    if ( ! acc.includes(curr) ) acc.push(curr);
+    return acc;
+}, []);
+
+const getCodiciRegioni = function (rows) {
+    return getUniqueValues(unpack(rows, CODICE_REGIONE_KEY))
+}
+
+const getRegioni = function (rows) {
+    const codiciRegioni = getCodiciRegioni(rows)
+    const regioni = [];
+    $.each(codiciRegioni, function(index, codiceRegione) {
+        // find regione name
+        regioneRow = rows.find(row => row[CODICE_REGIONE_KEY] === codiceRegione)
+        // create object
+        regione = {}
+        regione.codice = codiceRegione
+        regione.nome = regioneRow[REGIONE_KEY]
+        // put object in array
+        regioni.push(regione)
+    })
+    return regioni;
+}
+
 const getTraces = function ( rows ) {
-    console.log('rows', rows)
+    console.log('rows', rows.length)
+    // for (var i=0; i < rows.length; i++) {
+    //     row = rows[i];
+    //     console.log('row', row)
+    // }
+}
+
+function unpack(rows, key) {
+    return rows.map(function(row) { return row[key]; });
+}
+
+const aggregateData = function (data, aggregate)
+{
+    switch (aggregate) {
+        case 'regione':
+            console.log('aggregating for regione');
+            /*
+            FB_aggregateDateArray = FB_dateArray.map(function(date) { return date.split('-')[0]; });
+            TA_aggregateDateArray = TA_dateArray.map(function(date) { return date.split('-')[0]; });
+            */
+            socials_aggregate_date_array = data.map(
+                date_array => date_array.map(
+                    date => date.split('-')[0]
+                )
+            );
+            console.log(socials_aggregate_date_array);
+            tick_step = "M12";
+            break;
+    }
 }
